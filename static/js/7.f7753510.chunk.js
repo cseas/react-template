8b15247e3e5573dea705c30a97d0c6f39add8c6a
Abhijeet Singh <contact.abhijeetsingh@gmail.com>
2022-02-10 23:16:43 +0530

    Updates

---
+++ static/js/7.f7753510.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-cra-express"]=this["webpackJsonpreact-cra-express"]||[]).push([[7],{83:function(e,n,r){"use strict";r.r(n),r.d(n,"default",(function(){return c}));var t=r(1);function c(){var e;return Object(t.jsxs)(t.Fragment,{children:[Object(t.jsx)("h1",{children:"Server data"}),Object(t.jsxs)("p",{children:["You are in the ",null===(e=window.SERVER_DATA)||void 0===e?void 0:e.environment," environment"]})]})}}}]);
+//# sourceMappingURL=7.f7753510.chunk.js.map
